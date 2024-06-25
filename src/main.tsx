965db13c9539d6cb4df2bcae218501253082f066
ajin <aj5210@naver.com>
2024-06-25 20:40:33 +0900

    remove: 사용하지 않는 로고 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -7,9 +6,7 @@ const rootElement = document.getElementById('root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
       <App />
-    </React.StrictMode>,
   );
 } else {
   console.error('Root element가 존재하지 않습니다.');
