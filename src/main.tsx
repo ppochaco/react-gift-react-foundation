39d60ed0a946f65658ccd6cbdcab6b095bfd08c3
ajin <aj5210@naver.com>
2024-06-26 22:47:45 +0900

    rename(reset.css): css파일 styles 폴더로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
-import './reset.css';
+import '@/styles/reset.css';
 
 const rootElement = document.getElementById('root');
 
