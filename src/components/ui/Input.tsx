b850600106bfeede0b0705a9a58df46b88362077
ajin <aj5210@naver.com>
2024-06-28 17:42:18 +0900

    feat(components/ui): 공통 input 컴포넌트 추가

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,91 @@
+import { CSSObject, css } from '@emotion/react';
+import { InputHTMLAttributes, forwardRef } from 'react';
+
+const inputStateStyles: {
+  [key in 'default' | 'invalid' | 'disabled']: CSSObject;
+} = {
+  default: {
+    color: '#222222',
+    '&:focus': {
+      borderColor: '#000000',
+    },
+  },
+  invalid: {
+    borderColor: '#f99393',
+    '&:focus': {
+      borderColor: '#ff0000',
+    },
+  },
+  disabled: {
+    color: '#aaaaaa',
+    backgroundColor: '#f5f5f5',
+    cursor: 'not-allowed',
+  },
+};
+
+const inputSizeStyles: {
+  [key in 'sm' | 'md' | 'response']: CSSObject;
+} = {
+  sm: {
+    height: '2rem',
+    fontSize: '0.875rem',
+  },
+  md: {
+    height: '2.25rem',
+    fontSize: '1rem',
+  },
+  response: {
+    height: '2rem',
+    fontSize: '0.875rem',
+    '@media (min-width: 768px)': {
+      height: '2.25rem',
+      fontSize: '1rem',
+    },
+    '@media (min-width: 1024px)': {
+      width: '16rem',
+    },
+  },
+};
+
+interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  size?: 'sm' | 'md' | 'response';
+  invalid?: boolean;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className = '',
+      size = 'sm',
+      invalid = false,
+      disabled = false,
+      ...props
+    },
+    ref
+  ) => {
+    const inputStyle = css({
+      borderBottom: '1px solid #cccccc',
+      outline: 'none',
+      padding: '0 0.5rem',
+      ...inputSizeStyles[size],
+      ...(disabled && inputStateStyles.disabled),
+      ...(invalid && inputStateStyles.invalid),
+      ...(!disabled && !invalid && inputStateStyles.default),
+    });
+
+    return (
+      <input
+        className={className}
+        disabled={disabled}
+        css={inputStyle}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export default Input;
