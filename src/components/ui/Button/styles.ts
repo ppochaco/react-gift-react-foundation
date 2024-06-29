79f8754c15d81315a37890faf48c814f925afd20
ajin <aj5210@naver.com>
2024-06-29 21:22:29 +0900

    refactor(components/ui): 버튼 컴포넌트 스타일 리팩토링
    - 색상 팔레트 추가

--- src/components/ui/Button/styles.ts
+++ src/components/ui/Button/styles.ts
@@ -1,47 +1,39 @@
 import { CSSObject, css } from '@emotion/react';
 
+import { colors } from '@/styles/theme';
 import { Size, Theme } from '@/types/uiTypes';
 
-export const themeStyles: {
-  [key in 'kakao' | 'outline' | 'black' | 'lightGray' | 'darkGray']: CSSObject;
-} = {
-  kakao: {
-    backgroundColor: '#FEE501',
-    color: '#3C1D1E',
-    '&:hover': {
-      backgroundColor: '#f2da00',
-    },
-  },
-  outline: {
-    backgroundColor: 'transparent',
-    color: '#222222',
-    border: '1.2px solid #e0e0e0',
-    '&:hover': {
-      backgroundColor: '#efefef',
-    },
-  },
-  black: {
-    backgroundColor: '#000000',
-    color: '#FFFFFF',
-    '&:hover': {
-      backgroundColor: '#222222',
-    },
-  },
-  lightGray: {
-    backgroundColor: '#dddddd',
-    color: '#000000',
-    '&:hover': {
-      backgroundColor: '#cccccc',
-    },
-  },
-  darkGray: {
-    backgroundColor: '#222222',
-    color: '#FFFFFF',
-    '&:hover': {
-      backgroundColor: '#1a1a1a',
-    },
-  },
+const backgroundColor = {
+  kakao: colors.yellow[100],
+  outline: 'transparent',
+  black: colors.black,
+  lightGray: colors.gray[200],
+  darkGray: colors.gray[400],
 };
+
+const textColor = {
+  kakao: colors.gray[500],
+  outline: colors.gray[500],
+  black: colors.white,
+  lightGray: colors.gray[500],
+  darkGray: colors.white,
+};
+
+const hoverBackgroundColor = {
+  kakao: colors.yellow[200],
+  outline: colors.gray[100],
+  black: colors.gray[500],
+  lightGray: colors.gray[300],
+  darkGray: colors.gray[500],
+};
+
+export const themeStyles = (theme: Theme) => ({
+  backgroundColor: backgroundColor[theme],
+  color: textColor[theme],
+  '&:hover': {
+    backgroundColor: hoverBackgroundColor[theme],
+  },
+});
 
 export const sizeStyles: {
   [key in 'small' | 'large' | 'response']: CSSObject;
@@ -55,8 +47,8 @@ export const sizeStyles: {
     padding: '0 2rem',
   },
   response: {
-    padding: '0 1rem',
     height: '2rem',
+    padding: '0 1rem',
     '@media (min-width: 768px)': {
       height: '2.5rem',
       padding: '0 2rem',
@@ -70,6 +62,7 @@ export const sizeStyles: {
 export const buttonStyle = (theme: Theme, size: Size) =>
   css({
     borderRadius: '0.25rem',
-    ...themeStyles[theme],
+    border: theme === 'outline' ? `1.2px solid ${colors.gray[200]}` : '',
+    ...themeStyles(theme),
     ...sizeStyles[size],
   });
